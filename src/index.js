c3d6441a5cfb2a8766cb57db4b641feeb9d67316
LucianoPiantavinhaRoza <luciano.piantavinha@gmail.com>
2020-11-29 14:53:45 -0300

    bug 3

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
-import config from './aws-exports.js';
+import awsExports from './aws-exports';
 
-Amplify.configure(config);
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
